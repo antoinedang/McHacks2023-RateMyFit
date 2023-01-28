d742b32b943fb72974d92b899347b85518120917
rudolfKischer <rudikischer@gmail.com>
2023-01-28 16:05:34 -0500

    add-server-image-endpoint-and-client

--- server/app.py
+++ server/app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-from os import path
+from os import path, environ
+import logging
+import sys
 
 from flask import Flask
 import settings
 
 from blueprints.mainpage import fit_api
+from blueprints.client import client_api
 
 app = Flask(__name__)
 
@@ -12,10 +15,19 @@ def construct_app():
     app_dir = path.dirname(path.realpath(__file__))
     app_name = path.basename(app_dir)
 
-    app = Flask(app_name, root_path=app_dir, static_url_path='/static')
+    app = Flask(app_name, root_path=app_dir)
 
 
     app.register_blueprint(fit_api)
+    app.register_blueprint(client_api)
+
+    app.config.from_object('settings')
+
+    # Logging
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    logging.basicConfig(level=logging.DEBUG)
+    app.logger.setLevel(app.config.get('LOG_LEVEL', logging.INFO))
+    app.debug = True
 
     return app
 
@@ -23,5 +35,6 @@ def construct_app():
 app = construct_app()
 
 if __name__ == '__main__':
-    config = app
-    app.run(host='0.0.0.0', port=5002)
+    environ['FLASK_DEBUG'] = 'development'
+    config = app.config
+    app.run(debug=True,host='0.0.0.0', port=config['APP_PORT'])
