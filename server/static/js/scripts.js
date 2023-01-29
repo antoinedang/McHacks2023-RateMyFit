5bcd6f9fb243d23e6ddf63286845a2704f6a0015
Elie-Dimitri Abdo <elie-dimitri.abdo@mail.mcgill.ca>
2023-01-29 11:23:50 -0500

    Ll

--- server/static/js/scripts.js
+++ server/static/js/scripts.js
@@ -100,7 +100,7 @@ imageSelectButton.addEventListener("click", () => {
         body: formData,
         mode: 'no-cors'
     };
-    fetch( 'http://10.0.0.138:5002/fit', options )
+    fetch( 'http://192.168.2.39:5002/fit', options )
         .then( function(response) { return response.json() } )
         .catch(error => {console.error(error)})
         .then( function(data) {
